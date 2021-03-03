b4688155d1f8c7a185f01e3ded4e9301ba51de95
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-03 10:51:45 +0300

    style flash messages

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vvndkdsj237nbntog87kkvd'
 
+bootstrap.init_app(app)
 
 from app import views
